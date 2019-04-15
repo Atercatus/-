cd64bca123516409182b0ea45ea6efd6e325842d
AterCatus <myleelee2000@nate.com>
2019-04-16 03:05:23 +0900

    solve squarenono

--- 백준/제곱ㄴㄴ수(1016)/제곱ㄴㄴ수(1016)/main.cpp
+++ 백준/제곱ㄴㄴ수(1016)/제곱ㄴㄴ수(1016)/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 
 using namespace std;
 
-bool cache[10000001];
+#define MAX_SIZE 1000001
+
+bool cache[MAX_SIZE];
+
+long long findNoNoSquare(long long min, long long max) {
+	long long cnt = 0;
+
+	for (long long i = 2; i <= sqrt(max); i++) {
+		long long square = i * i;
+		long long start = min;
+		if (start % square != 0) {
+			start = (start / square + 1) * square;
+		}
+
+		for (long long next = start; next <= max; next += square) {
+			if (!cache[next - min]) {
+				cache[next - min] = true;
+				cnt++;
+			}
+		}
+	}
+
+	return max - min + 1 - cnt;
+}
 
 int main() {
 	long long min;
@@ -11,22 +35,10 @@ int main() {
 
 	cin >> min >> max;
 
-	for (int i = 0; i < 10000001; i++)
-		cache[i] = true;
+	for (long long i = 0; i < MAX_SIZE; i++)
+		cache[i] = false;
 
-	int i = 2;
-	if (min == 1) {
-		i = 3;
-		cache[0] = false;
-	}
-	for (i; i <= sqrt(max - min); i++) {
-		int n = min + i - 2;
-		while (n * n <= max) {
-			cache[]
-		}
-	}
-
-	// min이 1일때 구분
+	cout << findNoNoSquare(min, max) << endl;
 
 	return 0;
 }
