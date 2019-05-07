0c06dd8a1fc60b177d6af1c949d81b063cbac6dd
AterCatus <myleelee2053@naver.com>
2019-05-07 13:43:32 +0900

    Slove ASYMTILING

--- 알고스팟/ASYMTILING/ASYMTILING/main.cpp
+++ 알고스팟/ASYMTILING/ASYMTILING/main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 #define MOD 1000000007LL
 
 long long cache[MAX_SIZE];
+int len;
 
 long long put_tile(int width) {
 	if (width <= 1)
@@ -20,20 +21,37 @@ long long put_tile(int width) {
 	return ret = (put_tile(width - 2) + put_tile(width - 1)) % MOD;
 }
 
+long long find_asymtiling(int width) {
+	long long ret = 0;
+
+	if (width % 2 == 1) {
+		return ((put_tile(len) - put_tile(width/2) + MOD) % MOD);
+	}
+
+	int mid = width / 2;
+	ret = put_tile(width);
+	ret = (ret - put_tile(mid) + MOD) % MOD;
+	ret = (ret - put_tile(mid-1) + MOD) % MOD;
+	
+	return ret;
+}
+
 int main() {
 	int c;
 	cin >> c;
 	vector<long long> ret;
-	int len;
 
 	for (int i = 0; i < c; i++) {
 		cin >> len;
 		for (int j = 0; j <= len; j++) {
 			cache[j] = -1;
 		}
-		ret.push_back((put_tile(len)));
+		put_tile(len);
+		ret.push_back(find_asymtiling(len));
 	}
 
 	for (int i = 0; i < ret.size(); i++)
 		cout << ret[i] << endl;
+
+	return 0;
 }
